257b7c119b9da6416d8a8ed5af317ec8768a1a06
sarahoco014 <soconnor2@outlook.com>
2023-08-08 19:24:03 +0100

    amends data loader

--- src/main/java/com/bnta/chocolate/components/DataLoader.java
+++ src/main/java/com/bnta/chocolate/components/DataLoader.java
@@ -1,7 +1,9 @@
 package com.bnta.chocolate.components;
 
 import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.models.Estate;
 import com.bnta.chocolate.repositories.ChocolateRepository;
+import com.bnta.chocolate.repositories.EstateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -17,10 +19,17 @@ import java.util.List;
         @Autowired
         ChocolateRepository chocolateRepository;
 
+        @Autowired
+        EstateRepository estateRepository; // need to autowire this in too so that we can reference the Estate class
+
         @Override
         public void run(ApplicationArguments args) throws Exception {
 
-            List<String> rawWords = Arrays.asList(
+            Estate defaultEstate = new Estate("Default Estate", "Unknown"); // Create a default estate: we need this for the new Chocolate on line 43 as it takes in an Estate object as a parameter
+
+            estateRepository.save(defaultEstate); // saves default estate
+
+            List<String> chocolateTypes = Arrays.asList(
 
                     "snickers",
                     "mars",
@@ -30,11 +39,10 @@ import java.util.List;
                     "milkyway"
             );
 
-            for (String rawWord : rawWords) {
-                Chocolate chocolate = new Chocolate(rawWord);
-                chocolateRepository.save(chocolate);
+            for (String chocolateType : chocolateTypes) { // loop through the chocolate types (snickers, mars etc.)
+                Chocolate chocolate = new Chocolate(chocolateType, 70, defaultEstate);  // creates a new instance of the Chocolate class and initializes its properties using the provided values
+                chocolateRepository.save(chocolate); // saves chocolate
             }
-
         }
     }
 
