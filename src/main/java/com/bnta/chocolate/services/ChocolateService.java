2e18b035f34fb63df8e01c1ad7611332a2ac0ee1
sarahoco014 <soconnor2@outlook.com>
2023-08-08 17:32:24 +0100

    adds save methods

---
+++ src/main/java/com/bnta/chocolate/services/ChocolateService.java
@@ -0,0 +1,19 @@
+package com.bnta.chocolate.services;
+
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChocolateService {
+
+    @Autowired
+    ChocolateRepository chocolateRepository;
+
+    public Chocolate saveChocolate(Chocolate chocolate) {
+        chocolateRepository.save(chocolate); // saves chocolate with id attached to it
+        return chocolate;
+    }
+
+}
---
+++ src/main/java/com/bnta/chocolate/services/EstateService.java
@@ -0,0 +1,19 @@
+package com.bnta.chocolate.services;
+
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.models.Estate;
+import com.bnta.chocolate.repositories.EstateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstateService {
+
+    @Autowired
+    EstateRepository estateRepository;
+
+    public Estate saveEstate(Estate estate) {
+        estateRepository.save(estate); // saves chocolate with id attached to it
+        return estate;
+    }
+}
